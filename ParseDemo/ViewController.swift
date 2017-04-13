4062b3f5a703ff2104736f76f2191250bf9dbdab
Balaji Tummala <Balaji_Tummala@intuit.com>
2017-04-12 21:00:24 -0700

    ParseDemo

--- ParseDemo/ViewController.swift
+++ ParseDemo/ViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    let alertController = UIAlertController(title: "Succcess", message: "You have logined", preferredStyle: .alert)
+    
+    @IBAction func Login(_ sender: Any) {
+        let username = emailLabel.text
+        let password = passwordLabel.text
+        
+        PFUser.logInWithUsername(inBackground: username!, password: password!, block: {(user, error) in
+            if  user != nil {
+                print("Succeeded Login")
+                self.performSegue(withIdentifier: "showChatRoom", sender: self)
+            }
+        })
+    }
+    
+    @IBAction func SignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = emailLabel.text
+        user.password = passwordLabel.text
+        
+        user.signUpInBackground(block: {(succeeded, error) in
+            if succeeded {
+                print("Succeeded Singup")
+            } else {
+                print("Error")
+            }
+        })
+    }
+    
+    @IBOutlet weak var passwordLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
